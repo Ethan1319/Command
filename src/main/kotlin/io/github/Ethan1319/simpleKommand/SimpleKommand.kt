be040aabe8e26da1429dd53dc563311eba6cf4ae
Ethan1319 <simwj1234@gmail.com>
2025-06-14 17:01:05 +0900

    first commit

---
+++ src/main/kotlin/io/github/Ethan1319/simpleKommand/SimpleKommand.kt
@@ -0,0 +1,14 @@
+package io.github.Ethan1319.simpleKommand
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimpleKommand : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
